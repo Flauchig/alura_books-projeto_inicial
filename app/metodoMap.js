a2ae8c7ff2b70ff14d84ef2e5f7ad008814ae938
flauchig <rafael.vasconcellos0328@gmail.com>
2023-11-06 01:56:28 -0300

    organizando o codigo e colocando  um comando de desconto usando o .map do javascript

---
+++ app/metodoMap.js
@@ -0,0 +1,15 @@
+function aplicarDesconto(livros ){
+    const desconto  = 0.3;
+
+    livorsComDesconto  = livros.map(livro => {
+                // aqui estou trabalhado com  arraow functions
+
+                return {...livro, preco: livro.preco -(livro.preco * desconto )  }
+
+
+
+    } )
+
+    return livorsComDesconto
+
+}
